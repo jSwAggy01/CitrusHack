18bc9718d7aee1de068ff4c78ce2d78af8828a05
Derrick-Mao <derrickmao03@gmail.com>
2023-04-29 16:44:23 -0700

    Finished Exercise.h

--- Exercise.h
+++ Exercise.h
@@ -5,40 +5,20 @@ using namespace std;
 class Exercise 
 {
     public:
-        Exercise() : equipment("none"), muscle("none")
-        {
-        }
-
-        Exercise(string eq, string mu) : equipment(eq), muscle(mu)
-        {
-        }
-
         //output workout name, level, desc
-        void printWorkout() const {
+        Exercise() : equipment("none"), muscle("none") {}
+        Exercise(string eq, string musc) : equipment(eq), muscle(musc) {}
+
+        void PrintWorkout() const {
             cout << "Workout: " << workout << endl;
             cout << "Level of Difficulty: " << level << endl;
             cout << "Workout description: " << description << endl;
         }
 
-        //Goes to NonEq.h or Eq.h?
-        void option() 
-        {
-            //if user prompted for none prompt them again for calisthenics or cardio
-            if (equipment == "None") {
-                string nonEq;
-                cout << "Calisthenics | Cardio" << endl;
-                cin >> nonEq;
-            }
-
-            //goes into Eq.h to respective cate
-            else
-            {
-
-            }
-        }
     private:
         string equipment;
         string muscle;
+
         string workout;
         int level;
         string description;
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 <<<<<<< HEAD
 
 using namespace std;
@@ -6,8 +7,25 @@ using namespace std;
 =======
 using namespace std;
 >>>>>>> d356b6f443f2aa2ec1cd17060b6e5613c5d64be1
+
+void Prompt() 
+{
+    string eq;
+    string musc;
+
+    cout << "What equipments u got cuh?" << endl;
+    cout << "Calisthenics | Cardio | Weight | Machine" << endl;
+    cin >> eq;
+    cout << "What muscle u trynna work on?" << endl;
+    cin >> musc;
+
+    Exercise workOut1 = new Exercise(eq, musc);
+}
+
 int main()
 {
-    cout <<"Hello World"<<endl;
+    prompt();
+
     return 0;
 }
+
