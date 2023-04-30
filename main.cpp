3f0749cbc7dbb6f1d5a1ddc47fba9d7d917f0522
Derrick-Mao <derrickmao03@gmail.com>
2023-04-29 19:28:27 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include "Calisthenics.h"
+#include "Cardio.h"
+#include "Weight.h"
+#include "Machine.h"
+using namespace std;
+
+void Prompt() 
+{
+    string eq;
+    int lv;
+    string musc;
+
+    cout << "What equipments u got cuh?" << endl;
+    cout << "Calisthenics | Cardio | Weight | Machine" << endl;
+    cin >> eq;
+    cout << "What level u at?" << endl;
+    cout << "Level: 1 | 2 | 3" << endl;
+    cin >> lv;
+    cout << "What muscle u trynna work on?" << endl;
+    cin >> musc;
+
+    //Exercise workOut1 = new Exercise(eq, lvl, musc);
+    if (eq == "Calisthenics") 
+    {
+        Calisthenics workout = new Calisthenics(lv, musc);
+    }
+
+    else if (eq == "Cardio") 
+    {
+        Cardio workout = new Workout(lv, musc);
+    }
+
+    else if (eq == "Weight") 
+    {
+        Weight workout = new Workout(lv, musc);
+    }
+
+    else if (eq == "Machine") {
+        Machine workout = new Machine(lv, musc);
+    }
+}
+
+int main()
+{
+    prompt();
+
+    return 0;
+}
+
